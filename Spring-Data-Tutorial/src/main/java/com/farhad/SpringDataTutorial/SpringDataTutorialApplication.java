1d7689f0733b8a0042d7d6955b2a8e5b56e15aca
farhad-ahmed <frhd.ahmd@gmail.com>
2020-01-12 21:39:39 -0600

    the first step

---
+++ Spring-Data-Tutorial/src/main/java/com/farhad/SpringDataTutorial/SpringDataTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.farhad.SpringDataTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataTutorialApplication.class, args);
+	}
+
+}
